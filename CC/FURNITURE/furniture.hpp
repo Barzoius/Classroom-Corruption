3af8bf4ec5f101b2880b5b7525deab886da055c9
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-05 07:52:46 -1100

    Update furniture.hpp

--- CC/FURNITURE/furniture.hpp
+++ CC/FURNITURE/furniture.hpp
@@ -1,1 +1,37 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <vector>
 
+class DESK{
+private:
+    SDL_Surface* image;
+    SDL_Rect pos;
+    SDL_Surface* desk_image;
+    SDL_Texture* desk_texture;
+
+public:
+
+
+    DESK();
+
+    void draw(SDL_Surface* dest);
+
+    bool LoadDesk(std::string& filename);
+
+    SDL_Rect getPos_y() const;
+
+    SDL_Texture getDeskTexture() const;
+    void setDeskTexture(SDL_Renderer* R);
+
+};
+
+class TEACHERS_DESK : public DESK{
+private:
+
+public:
+    TEACHERS_DESK()= default;
+};
+
+void DeskInit();
+void DeskQuit();
