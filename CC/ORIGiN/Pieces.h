69e7d5fe73b5efb67c31da1fb226172629404d28
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-08 07:57:00 -1100

    Update Pieces.h

--- CC/ORIGiN/Pieces.h
+++ CC/ORIGiN/Pieces.h
@@ -1,4 +1,4 @@
-#ifndef CLASSROOM_CORRUPTION_PIECES_H
+=#ifndef CLASSROOM_CORRUPTION_PIECES_H
 #define CLASSROOM_CORRUPTION_PIECES_H
 
 #endif //CLASSROOM_CORRUPTION_PIECES_H
@@ -23,6 +23,22 @@ public:
     void QUIT();
 
     Piece();
+    ~Piece();
+
+
+    void setRenderer(SDL_Renderer* ren);
+    void setImage(const std::string& filename);
+    void setTexture();
+
+    void setSize(int width, int height);
+    void setPosition(int x_coordinate, int y_coordinate , int z);
+
+    int getPosition_X() const;
+    int getPosition_Y() const;
+    int getPosition_W() const;
+    int getPosition_H() const;
+
+    SDL_Rect getRect() const;
 
 };
 
@@ -30,4 +46,9 @@ class Furniture : public Piece{};
 //class Desk : public Furniture{};
 
 
-class Player : public Piece{};
+class Player : public Piece{
+private:
+
+public:
+    void MOVE(int x, int y);
+};
