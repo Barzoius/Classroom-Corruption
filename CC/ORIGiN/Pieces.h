4b9daa1b7740b7aa42b8b0fe152295c5f2aa90b6
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-08 07:18:35 -1100

    Update Pieces.h

--- CC/ORIGiN/Pieces.h
+++ CC/ORIGiN/Pieces.h
@@ -1,1 +1,33 @@
+#ifndef CLASSROOM_CORRUPTION_PIECES_H
+#define CLASSROOM_CORRUPTION_PIECES_H
 
+#endif //CLASSROOM_CORRUPTION_PIECES_H
+
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+class Piece{
+private:
+    SDL_Window* window{};
+    SDL_Renderer* renderer{};
+    SDL_Rect position{};
+    SDL_Texture* texture{};
+    SDL_Surface* image{};
+
+public:
+    void INIT();
+    void QUIT();
+
+    Piece();
+
+};
+
+class Furniture : public Piece{};
+//class Desk : public Furniture{};
+
+
+class Player : public Piece{};
