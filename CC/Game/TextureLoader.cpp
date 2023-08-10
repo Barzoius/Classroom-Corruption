da738ac778baf856e5808d43050bf41b20dad1fb
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-10 05:14:58 -1100

    Update TextureLoader.cpp

--- CC/Game/TextureLoader.cpp
+++ CC/Game/TextureLoader.cpp
@@ -1,13 +1,1 @@
-#ifndef CLASSROOM_CORRUPTION_TEXTURELOADER_H
-#define CLASSROOM_CORRUPTION_TEXTURELOADER_H
 
-#endif //CLASSROOM_CORRUPTION_TEXTURELOADER_H
-
-
-#include "SDL.h"
-#include "SDL_image.h"
-
-class TextureLoader{
-public:
-    static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* ren);
-};
