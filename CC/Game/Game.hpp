157774156ac52c80876f507fdf7f9a22da95629f
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-10 04:58:23 -1100

    Update Game.hpp

--- CC/Game/Game.hpp
+++ CC/Game/Game.hpp
@@ -1,31 +1,35 @@
-#pragma once
-#include "SDL.h"
-#include <stdio.h>
+#ifndef CLASSROOM_CORRUPTION_GAME_H
+#define CLASSROOM_CORRUPTION_GAME_H
+
+#endif //CLASSROOM_CORRUPTION_GAME_H
+
+#include "SDL2/SDL.h"
+#include "SDL_image.h"
 #include <iostream>
+#include <vector>
 
-class Game {
-
+class Game{
 private:
-	
-	int cnt = 0;
-	bool isRunning;
-	SDL_Window *window;
-	SDL_Renderer *renderer;
+    bool isRunning;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
 
 public:
 
-	Game();
-	~Game();
-
-	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
-
-	void handleEvents();
-	void update();
-	void render();
-	void clean();
-
-	bool running() { return isRunning; };
+    Game();
+    ~Game();
 
 
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() { return isRunning; };
+
+    SDL_Renderer* getRenderer() const { return renderer;}
+    SDL_Window* getWindow() const { return window;}
 
 };
