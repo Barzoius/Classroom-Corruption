b9465e0267f90a501ea6f9eb4ae7f2ccfc402ee7
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-17 02:48:40 -1100

    Update TextureLoader.cpp

--- CC/Game/TextureLoader.cpp
+++ CC/Game/TextureLoader.cpp
@@ -1,10 +1,15 @@
+#pragma once
 #include "TextureLoader.h"
 
-SDL_Texture* TextureLoader::LoadTexture(const char *filename, SDL_Renderer *ren) {
+SDL_Texture* TextureLoader::LoadTexture(const char *filename) {
 
     SDL_Surface* surface = IMG_Load(filename);
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surface);
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(Game::renderer, surface);
     SDL_FreeSurface(surface);
 
     return tex;
+}
+
+void TextureLoader::Draw(SDL_Texture *tex, SDL_Rect src, SDL_Rect dest) {
+    SDL_RenderCopy(Game::renderer, tex, &src, & dest);
 }
