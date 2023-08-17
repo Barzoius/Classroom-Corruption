1f1ecf6bebd3e770992b8bc3cd7905cd6ba96dc5
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-17 02:47:55 -1100

    Update Game.cpp

--- CC/Game/Game.cpp
+++ CC/Game/Game.cpp
@@ -1,85 +1,100 @@
-#include "Game.hpp"
+#include "GameOBJ.h"
+#include "Map.h"
 
+GameOBJ* Player1;
+GameOBJ* Player2;
+Map* map1;
+
+SDL_Renderer* Game::renderer = nullptr;
 
 Game::Game()
 {
 
 }
 
-Game::~Game() 
+Game::~Game()
 {
 
 }
 
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
-	int flags = 0;
-	
-	if (fullscreen)
-	{
-		flags = SDL_WINDOW_FULLSCREEN;
-	}
+    int flags = 0;
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-	{
-		std::cout << "SUBSYSTEMS" << std::endl;
-		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+    if (fullscreen)
+    {
+        flags = SDL_WINDOW_FULLSCREEN;
+    }
 
-		if (window)
-		{
-			std::cout << "WINDOW CREATED" << std::endl;
-		}
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
 
-		renderer = SDL_CreateRenderer(window, -1, 0);
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
-		if (renderer)
-		{
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			std::cout << "RENDERER CREATED" << std::endl;
-		}
+        if (window)
+        {
+            std::cout << "WINDOW CREATED" << std::endl;
+        }
 
-		isRunning = true;
-	}
-	else {
-		isRunning = false;
-	}
+        renderer = SDL_CreateRenderer(window, -1, 0);
+
+        if (renderer)
+        {
+            SDL_SetRenderDrawColor(renderer, 27, 27, 27, 255);
+            std::cout << "RENDERER CREATED" << std::endl;
+        }
+
+        isRunning = true;
+    }
+    else {
+        isRunning = false;
+    }
+
+    Player2 = new GameOBJ(R"(C:\Users\mecca\CLionProjects\ClassRoom_Corruption\CRT.png)", 0, 0);
+    Player1 = new GameOBJ(R"(C:\Users\mecca\CLionProjects\ClassRoom_Corruption\CRT.png)", 50, 50 );
+    map1 = new Map();
 }
 
 void Game::handleEvents()
 {
-	SDL_Event event;
-	SDL_PollEvent(&event);
+    SDL_Event event;
+    SDL_PollEvent(&event);
 
-	switch (event.type)
-	{
-		case SDL_QUIT:
-			isRunning = false;
-			break;
+    switch (event.type)
+    {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
 
-		default: break;
-	}
+        default: break;
+    }
 }
 
-void Game::update()
-{
-	cnt++;
-	std::cout << cnt << std::endl;
+void Game::update(){
+    Player2->Update();
+    Player1 ->Update();
+//    map1 ->
 }
 
 void Game::render()
 {
-	SDL_RenderClear(renderer);
-	
-	SDL_RenderPresent(renderer);
+
+    SDL_RenderClear(renderer);
+    map1 -> DrawMap();
+    Player2 -> Render();
+    Player1 -> Render();
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::clean()
 {
-	SDL_DestroyWindow(window);
+    SDL_DestroyWindow(window);
 
-	SDL_DestroyRenderer(renderer);
+    SDL_DestroyRenderer(renderer);
 
-	SDL_Quit();
+    SDL_Quit();
+    IMG_Quit();
 
-	std::cout << "GAME CLEANED" << std::endl;
+    std::cout << "GAME CLEANED" << std::endl;
 }
