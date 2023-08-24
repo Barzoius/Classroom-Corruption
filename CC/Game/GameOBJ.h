816071eb3e47ea2c465012558f26fe101bf2e369
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-23 22:35:01 -1100

    Update GameOBJ.h

--- CC/Game/GameOBJ.h
+++ CC/Game/GameOBJ.h
@@ -1,1 +1,37 @@
+#ifndef CLASSROOM_CORRUPTION_GAME_H
+#define CLASSROOM_CORRUPTION_GAME_H
 
+#endif //CLASSROOM_CORRUPTION_GAME_H
+#pragma once
+#include "SDL2/SDL.h"
+#include "SDL_image.h"
+#include <iostream>
+#include <vector>
+
+
+class Game{
+private:
+    bool isRunning;
+    SDL_Window* window;
+
+public:
+
+    static SDL_Renderer * renderer;
+
+    Game();
+    ~Game();
+
+
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() { return isRunning; };
+
+//    SDL_Renderer* getRenderer() const { return renderer;}
+    SDL_Window* getWindow() const { return window;}
+
+};
