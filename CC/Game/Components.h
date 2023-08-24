d429eea46b820ea2ec88aad7a0f133a9e3b376e5
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-23 22:36:19 -1100

    Update Components.h

--- CC/Game/Components.h
+++ CC/Game/Components.h
@@ -1,1 +1,11 @@
+#ifndef CLASSROOM_CORRUPTION_COMPONENTS_H
+#define CLASSROOM_CORRUPTION_COMPONENTS_H
+
+#endif //CLASSROOM_CORRUPTION_COMPONENTS_H
+
+#pragma once
+
+#include "ECS.h"
+#include "TransformComp.h"
+#include "SpriteComp.h"
 
