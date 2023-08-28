8e219e0ee7b7281ff9e383c14cfb0637d1d47d37
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-08-28 03:24:25 -1100

    Update KeyBoardControls.h

--- CC/Game/ECS/KeyBoardControls.h
+++ CC/Game/ECS/KeyBoardControls.h
@@ -21,23 +21,22 @@ public:
             switch(Game::event.key.keysym.sym)
             {
                 case SDLK_w:
-                    transform -> velocity.y = -1;
-                    break;
-
                 case SDLK_UP:
                     transform -> velocity.y = -1;
                     break;
-                
 
                 case SDLK_a:
+                case SDLK_LEFT:
                     transform -> velocity.x = -1;
                     break;
 
                 case SDLK_s:
+                case SDLK_DOWN:
                     transform -> velocity.y = 1;
                     break;
 
                 case SDLK_d:
+                case SDLK_RIGHT:
                     transform -> velocity.x = 1;
                     break;
                 default:
@@ -50,23 +49,22 @@ public:
             switch(Game::event.key.keysym.sym)
             {
                 case SDLK_w:
-                    transform -> velocity.y = 0;
-                    break;
-
                 case SDLK_UP:
                     transform -> velocity.y = 0;
                     break;
-                
-                    
+
                 case SDLK_a:
+                case SDLK_LEFT:
                     transform -> velocity.x = 0;
                     break;
 
                 case SDLK_s:
+                case SDLK_DOWN:
                     transform -> velocity.y = 0;
                     break;
 
                 case SDLK_d:
+                case SDLK_RIGHT:
                     transform -> velocity.x = 0;
                     break;
                 default:
