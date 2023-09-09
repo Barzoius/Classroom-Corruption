278cc0e35e2da704f99a6e642246b4352fbfffcb
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-09-09 07:08:23 -1100

    Update TileComponent.h

--- CC/Game/ECS/TileComponent.h
+++ CC/Game/ECS/TileComponent.h
@@ -1,1 +1,25 @@
+#pragma once
 
+#include "ECS.h"
+#include "TransformComp.h"
+#include "SDL.h"
+
+class TileComponent : public Component{
+public:
+    TransformComponent* transform;
+    SpriteComponent* sprite;
+    SDL_Rect tileRect;
+    char* path;
+    
+    int tileID;
+
+    TileComponent() = default;
+
+    TileComponent(int x, int y, int w, int h, int id){
+        tileRect.x = x;
+        tileRect.y = y;
+        tileRect.w = w;
+        tileRect.h = h;
+        tileID = id;
+    }
+};
