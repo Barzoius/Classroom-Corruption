4a9cea7a935d03a44958256799dc8195effe3220
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-09-11 07:21:16 -1100

    Update Vector2D.h

--- CC/Game/Vector2D.h
+++ CC/Game/Vector2D.h
@@ -1,1 +1,33 @@
+#pragma once
 
+#include <iostream>
+
+class Vector2D
+{
+public:
+    float x, y;
+
+    Vector2D();
+    Vector2D(float x, float y);
+
+    Vector2D& Add(const Vector2D& vec);
+    Vector2D& Sub(const Vector2D& vec);
+    Vector2D& Mult(const Vector2D& vec);
+    Vector2D& Div(const Vector2D& vec);
+
+    friend Vector2D& operator + (Vector2D& vec1, const Vector2D& vec2);
+    friend Vector2D& operator - (Vector2D& vec1, const Vector2D& vec2);
+    friend Vector2D& operator * (Vector2D& vec1, const Vector2D& vec2);
+    friend Vector2D& operator / (Vector2D& vec1, const Vector2D& vec2);
+
+    Vector2D& operator += (const Vector2D& vec);
+    Vector2D& operator -= (const Vector2D& vec);
+    Vector2D& operator *= (const Vector2D& vec);
+    Vector2D& operator /= (const Vector2D& vec);
+
+    Vector2D& operator *(const int& i /*scale*/);
+    Vector2D& Zero();
+
+    friend std::ostream& operator <<(std::ostream& out, const Vector2D& vec);
+
+};
