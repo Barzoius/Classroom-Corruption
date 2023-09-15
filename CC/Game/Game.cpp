a6e25f1b2020964c82485675b99c049ca0be5395
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-09-14 18:45:50 -1100

    Update Game.cpp

--- CC/Game/Game.cpp
+++ CC/Game/Game.cpp
@@ -15,15 +15,9 @@ Manager manager;
 auto& player(manager.addEntity());
 auto& wall(manager.addEntity());
 
-Game::Game()
-{
+Game::Game(){}
 
-}
-
-Game::~Game()
-{
-
-}
+Game::~Game(){}
 
 void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
